3b0f7b5e9fd1f7bfe14e5ea7300d3e28578ffd4a
JC <juliomanolotk@gmail.com>
2024-04-05 14:45:02 -0300

    Aula 04

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('helloWorld/', views.helloWorld),
+    path('', views.tasklist, name='task-list'),
+    path('yourname/<str:name>', views.yourname, name='your-name')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def helloWorld(request):
     return HttpResponse('hello world!')
+def tasklist(request):
+    return render(request, 'tasks/list.html')
+def yourname(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})
